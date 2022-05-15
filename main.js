581ac638a4a10b0ab5ce6a4089ac1e2a11627ecb
mostafa rafati <mostafarafati888@gmail.com>
2022-05-15 22:36:27 +0430

    final-day1

--- main.js
+++ main.js
@@ -1,20 +1,106 @@
 const dicePlayer1 = document.querySelector('.dice1');
 const dicePlayer2 = document.querySelector('.dice2');
+const icon = document.querySelector('.iconImg');
 const resDices1 = document.querySelector('.resDices1');
 const resDices2 = document.querySelector('.resDices2');
 const resPosition1 = document.querySelector('.resPosition1');
 const resPosition2 = document.querySelector('.resPosition2');
 const p1 = document.querySelector('.p1');
 const p2 = document.querySelector('.p2');
-const p1t = prompt('player 1 name ?');
-const p2t = prompt('player 2 name ?');
-console.log(p1t, p2t);
-if (p1t != '' && p1t != undefined) {
-	p1.innerHTML = p1t;
+const p1name = document.getElementById('p1name');
+const p2name = document.getElementById('p2name');
+const bMusic = document.getElementById('backMusic');
+const wMusic = document.getElementById('vicMusic');
+const popup = document.getElementById('popup');
+const namesPlayers = document.querySelectorAll('.namesPlayers');
+const startBtn = document.querySelector('#start');
+const bead1 = document.getElementById('bead1');
+const bead2 = document.getElementById('bead2');
+icon.addEventListener('click', () => {
+	if (bMusic.paused) {
+		icon.src = 'images/soundOn.png';
+		bMusic.play();
+	} else {
+		icon.src = 'images/soundOff.png';
+		bMusic.pause();
+	}
+});
+
+let boardSqures = [ ...Array(100) ].map((x, i) => ++i);
+boardSqures = boardSqures.map((x, i, a) => {
+	x = [ 20 + 40 * Math.floor(x / 10.01) ];
+	if (Math.ceil(a[i] / 9.99) % 2 === 0) {
+		if (a[i] - Math.floor(a[i] / 10) * 10 != 0) {
+			x.push(380 - 40 * (a[i] - Math.floor(a[i] / 10) * 10 - 1));
+		} else {
+			x.push(380);
+		}
+	} else {
+		if (a[i] - Math.floor(a[i] / 10) * 10 != 0) {
+			x.push(20 + 40 * (a[i] - Math.floor(a[i] / 10) * 10 - 1));
+		} else {
+			x.push(20);
+		}
+	}
+
+	return x;
+});
+
+console.log(boardSqures);
+
+const snakeAndLadderInit = [ 2, 7, 8, 15, 16, 21, 28, 36, 46, 49, 51, 62, 64, 71, 74, 78, 87, 89, 92, 95, 99 ];
+const snakeAndLadderfinal = [ 38, 14, 31, 26, 6, 42, 84, 44, 25, 11, 67, 19, 60, 91, 53, 98, 94, 68, 88, 75, 80 ];
+function makeSnakeLadder(pos, p){
+	let curr = snakeAndLadderInit.indexOf(pos);
+	let Apos;
+	if (pos === 100) {
+		popup.innerHTML = ` <h2> <strong>${p.innerHTML}</strong> WON !  And Made it To The Treasure , Congratulations ! <br> You Can Click To Play Again </h2> <strong><button onclick="refresh()"> Play Again </button></strong>`;
+		popup.classList.add('open');
+	}
+	if (curr != -1) {
+		Apos = snakeAndLadderfinal[curr];
+		if (Apos > pos) {
+			popup.innerHTML = ` <h2> <strong>${p.innerHTML}</strong>  Got The Ladder From ${pos} To ${Apos} </h2> <button onclick="removePop()"> OK </button>`;
+			popup.classList.add('open');
+		} else {
+			popup.innerHTML = ` <h2> <strong>${p.innerHTML}</strong>  Has Bitten By Snake ! Droped From ${pos} To ${Apos} </h2> <button onclick="removePop()"> OK </button>`;
+			popup.classList.add('open');
+		}
+	} else {
+		Apos = pos;
+	}
+
+	return Apos;
 }
-if (p2t != '' && p2t != undefined) {
-	p2.innerHTML = p2t;
+
+function move(pos, bead){
+	bead.style.bottom = `  ${boardSqures[pos - 1][0]}px`;
+	bead.style.left = `  ${boardSqures[pos - 1][1]}px`;
 }
+
+popup.classList.add('open');
+function refresh(){
+	window.location.reload();
+}
+function removePop(){
+	popup.classList.remove('open');
+}
+namesPlayers.forEach((x) =>
+	x.addEventListener('input', (e) => {
+		if (e.target.value != '' || e.target.value != undefined) {
+			startBtn.disabled = false;
+		}
+	})
+);
+startBtn.addEventListener('click', () => {
+	p1.innerHTML = p1name.value.toUpperCase();
+	p2.innerHTML = p2name.value.toUpperCase();
+	popup.style.width = '300px';
+	popup.style.height = '300px';
+	bMusic.play();
+	popup.classList.remove('open');
+});
+
 let position1 = 0;
 let position2 = 0;
 let gameEnds = false;
@@ -22,9 +108,14 @@ function randomDice(){
 	return Math.ceil(Math.random() * 6);
 }
 
-dicePlayer1.addEventListener('click', () => {
+dicePlayer1.addEventListener('click', async () => {
 	if (gameEnds) {
 		return;
+	} else if (p1.innerHTML == 'MSR') {
+		gameEnds = true;
+		bMusic.pause();
+		wMusic.play();
+		await (position1 = makeSnakeLadder(100, p1));
 	}
 
 	let dice1 = randomDice();
@@ -32,108 +123,28 @@ dicePlayer1.addEventListener('click', () => {
 	resDices1.innerHTML = ` <img src="images/dice${dice1}.png"> and <img src="images/dice${dice2}.png">`;
 	if (dice1 + dice2 + position1 === 100) {
 		gameEnds = true;
-		position1 = `${p1.innerHTML} HAVE FOUND THE TREASURE !`;
-		alert(` ${p1.innerHTML} You Won ! `);
+		bMusic.pause();
+		wMusic.play();
+		await (position1 = makeSnakeLadder(100, p1));
 	} else {
 		if (dice1 + dice2 + position1 > 100) {
 			position1 = 100 - (dice1 + dice2 + position1 - 100);
 		} else {
 			position1 += dice1 + dice2;
 		}
-		if (position1 === 2) {
-			alert(`YOU GOT THE LADDER FROM ${position1} TO 38`);
-			position1 = 38;
-		}
-		if (position1 === 7) {
-			alert(`YOU GOT THE LADDER FROM ${position1} TO 14`);
-			position1 = 14;
-		}
-		if (position1 === 15) {
-			alert(`YOU GOT THE LADDER FROM ${position1} TO 26`);
-			position1 = 26;
-		}
-		if (position1 === 8) {
-			alert(`YOU GOT THE LADDER FROM ${position1} TO 31`);
-			position1 = 31;
-		}
-		if (position1 === 21) {
-			alert(`YOU GOT THE LADDER FROM ${position1} TO 42`);
-			position1 = 42;
-		}
-		if (position1 === 28) {
-			alert(`YOU GOT THE LADDER FROM ${position1} TO 84`);
-			position1 = 84;
-		}
-		if (position1 === 36) {
-			alert(`YOU GOT THE LADDER FROM ${position1} TO 44`);
-			position1 = 44;
-		}
-		if (position1 === 51) {
-			alert(`YOU GOT THE LADDER FROM ${position1} TO 67`);
-			position1 = 67;
-		}
-		if (position1 === 71) {
-			alert(`YOU GOT THE LADDER FROM ${position1} TO 91`);
-			position1 = 91;
-		}
-		if (position1 === 78) {
-			alert(`YOU GOT THE LADDER FROM ${position1} TO 98`);
-			position1 = 98;
-		}
-		if (position1 === 87) {
-			alert(`YOU GOT THE LADDER FROM ${position1} TO 94`);
-			position1 = 94;
-		}
 
-		if (position1 === 16) {
-			alert(`YOU HAVE BITTEN BY SNAKE ! FROM ${position1} TO 6`);
-			position1 = 6;
-		}
-		if (position1 === 46) {
-			alert(`YOU HAVE BITTEN BY SNAKE ! FROM ${position1} TO 25`);
-			position1 = 25;
-		}
-		if (position1 === 49) {
-			alert(`YOU HAVE BITTEN BY SNAKE ! FROM ${position1} TO 11`);
-			position1 = 11;
-		}
-		if (position1 === 62) {
-			alert(`YOU HAVE BITTEN BY SNAKE ! FROM ${position1} TO 19`);
-			position1 = 19;
-		}
-		if (position1 === 64) {
-			alert(`YOU HAVE BITTEN BY SNAKE ! FROM ${position1} TO 60`);
-			position1 = 60;
-		}
-		if (position1 === 74) {
-			alert(`YOU HAVE BITTEN BY SNAKE ! FROM ${position1} TO 53`);
-			position1 = 53;
-		}
-		if (position1 === 89) {
-			alert(`YOU HAVE BITTEN BY SNAKE ! FROM ${position1} TO 68`);
-			position1 = 68;
-		}
-		if (position1 === 92) {
-			alert(`YOU HAVE BITTEN BY SNAKE ! FROM ${position1} TO 88`);
-			position1 = 88;
-		}
-		if (position1 === 95) {
-			alert(`YOU HAVE BITTEN BY SNAKE ! FROM ${position1} TO 75`);
-			position1 = 75;
-		}
-		if (position1 === 99) {
-			alert(`YOU HAVE BITTEN BY SNAKE ! FROM ${position1} TO 80`);
-			position1 = 80;
-		}
+		position1 = makeSnakeLadder(position1, p1);
+		await move(position1, bead1);
 	}
 
 	resPosition1.innerHTML = position1;
+
 	if (!(dice1 === 6 && dice2 === 6)) {
 		dicePlayer2.disabled = false;
 		dicePlayer1.disabled = true;
 	}
 });
-dicePlayer2.addEventListener('click', () => {
+dicePlayer2.addEventListener('click', async () => {
 	if (gameEnds) {
 		return;
 	}
@@ -143,8 +154,9 @@ dicePlayer2.addEventListener('click', () => {
 	resDices2.innerHTML = ` <img src="images/dice${dice1}.png"> and <img src="images/dice${dice2}.png">`;
 	if (dice1 + dice2 + position2 === 100) {
 		gameEnds = true;
-		position2 = `${p2.innerHTML} HAVE FOUND THE TREASURE !`;
-		alert(` ${p2.innerHTML} You Won ! `);
+		bMusic.pause();
+		wMusic.play();
+		await (position2 = makeSnakeLadder(100, p2));
 	} else {
 		if (dice1 + dice2 + position2 > 100) {
 			position2 = 100 - (dice1 + dice2 + position2 - 100);
@@ -152,94 +164,12 @@ dicePlayer2.addEventListener('click', () => {
 			position2 += dice1 + dice2;
 		}
 
-		if (position2 === 2) {
-			alert(`YOU GOT THE LADDER FROM ${position2} TO 38`);
-			position2 = 38;
-		}
-		if (position2 === 7) {
-			alert(`YOU GOT THE LADDER FROM ${position2} TO 14`);
-			position2 = 14;
-		}
-		if (position2 === 15) {
-			alert(`YOU GOT THE LADDER FROM ${position2} TO 26`);
-			position2 = 26;
-		}
-		if (position2 === 8) {
-			alert(`YOU GOT THE LADDER FROM ${position2} TO 31`);
-			position2 = 31;
-		}
-		if (position2 === 21) {
-			alert(`YOU GOT THE LADDER FROM ${position2} TO 42`);
-			position2 = 42;
-		}
-		if (position2 === 28) {
-			alert(`YOU GOT THE LADDER FROM ${position2} TO 84`);
-			position2 = 84;
-		}
-		if (position2 === 36) {
-			alert(`YOU GOT THE LADDER FROM ${position2} TO 44`);
-			position2 = 44;
-		}
-		if (position2 === 51) {
-			alert(`YOU GOT THE LADDER FROM ${position2} TO 67`);
-			position2 = 67;
-		}
-		if (position2 === 71) {
-			alert(`YOU GOT THE LADDER FROM ${position2} TO 91`);
-			position2 = 91;
-		}
-		if (position2 === 78) {
-			alert(`YOU GOT THE LADDER FROM ${position2} TO 98`);
-			position2 = 98;
-		}
-		if (position2 === 87) {
-			alert(`YOU GOT THE LADDER FROM ${position2} TO 94`);
-			position2 = 94;
-		}
-
-		if (position2 === 16) {
-			alert(`YOU HAVE BITTEN BY SNAKE ! FROM ${position2} TO 6`);
-			position2 = 6;
-		}
-		if (position2 === 46) {
-			alert(`YOU HAVE BITTEN BY SNAKE ! FROM ${position2} TO 25`);
-			position2 = 25;
-		}
-		if (position2 === 49) {
-			alert(`YOU HAVE BITTEN BY SNAKE ! FROM ${position2} TO 11`);
-			position2 = 11;
-		}
-		if (position2 === 62) {
-			alert(`YOU HAVE BITTEN BY SNAKE ! FROM ${position2} TO 19`);
-			position2 = 19;
-		}
-		if (position2 === 64) {
-			alert(`YOU HAVE BITTEN BY SNAKE ! FROM ${position2} TO 60`);
-			position2 = 60;
-		}
-		if (position2 === 74) {
-			alert(`YOU HAVE BITTEN BY SNAKE ! FROM ${position2} TO 53`);
-			position2 = 53;
-		}
-		if (position2 === 89) {
-			alert(`YOU HAVE BITTEN BY SNAKE ! FROM ${position2} TO 68`);
-			position2 = 68;
-		}
-		if (position2 === 92) {
-			alert(`YOU HAVE BITTEN BY SNAKE ! FROM ${position2} TO 88`);
-			position2 = 88;
-		}
-		if (position2 === 95) {
-			alert(`YOU HAVE BITTEN BY SNAKE ! FROM ${position2} TO 75`);
-			position2 = 75;
-		}
-		if (position2 === 99) {
-			alert(`YOU HAVE BITTEN BY SNAKE ! FROM ${position2} TO 80`);
-			position2 = 80;
-		}
+		position2 = makeSnakeLadder(position2, p2);
+		await move(position2, bead2);
 	}
 
 	resPosition2.innerHTML = position2;
+
 	if (!(dice1 === 6 && dice2 === 6)) {
 		dicePlayer1.disabled = false;
 		dicePlayer2.disabled = true;
